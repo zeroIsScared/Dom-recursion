bd7c802117ad4ff6c4bdc38a06cfbb7399c7da93
zeroIsScared <69678222+zeroIsScared@users.noreply.github.com>
2023-06-07 16:18:18 +0300

    Add files via upload
    refactor recursion with for each

--- dom-visit.js
+++ dom-visit.js
@@ -1,24 +1,31 @@
 'use strict';
 
-const visit = (element)=>{
+const visit = (element) => {
     //HW1. explain 'element.childNodes[0].textContent.trim()'
 
     //Here was used 'element.childNodes[0].textContent.trim()' because it was needed to capture the text content that is not an element 
-    //of given the element, and trim() was used to get rid of unnecessary space that is retuned alongside text when using textContent.
+    //of given element in case element has no child elements, and trim() was used to get rid of unnecessary space that is retuned alongside text
+    // when using textContent.
+
     console.log(
-            `visiting ${element.childNodes[0].textContent.trim()}`
+        `visiting ${element.childNodes[0].textContent.trim()}`
     )
 
+
     //HW2. refactor the code using Array.forEach()
-  //  for(let index =0; index < element.children.length; index++) {
-    //    visit(element.children[index]);
-   // }
-let elements= Array.from(element.children);
-console.log(elements)
-    elements.forEach(function (item, index) {
-        visit(item.children[index])
+    // for (let index = 0; index < element.children.length; index++) {
+    //     visit(element.children[index]);
+    // } 
+
+    let elements = [].slice.call(element.children);
+
+    elements.forEach((element) => {
+
+        visit(element);
+
     });
 }
 
-//// using DFS///
+
+// using DFS///
 visit(root);
